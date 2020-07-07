bd5a086c472d69ab737bddee7ebf951610f002f4
WBertan <william.bertan@gmail.com>
2020-07-07 16:22:58 +0100

    Add option to try with a Cipher.

--- app/src/main/java/com/test/biometrics/MainActivity.kt
+++ app/src/main/java/com/test/biometrics/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.test.biometrics
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricConstants
+import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
+import androidx.security.crypto.MasterKeys
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,18 +54,76 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.button).setOnClickListener { show() }
+        findViewById<Button>(R.id.button1).setOnClickListener { showWithoutCipher() }
+        findViewById<Button>(R.id.button2).setOnClickListener { showWithCipher() }
+
+        val canAuthenticate = BiometricManager.from(this)
+            .canAuthenticate() == BiometricManager.BIOMETRIC_SUCCESS
+        val hasBiometrics = hasBiometrics(this)
+        "canAuthenticate=$canAuthenticate | hasBiometrics=$hasBiometrics".writeResult()
     }
 
-    private fun show() {
+    private fun hasBiometrics(context: Context): Boolean {
+        val pm = context.packageManager
+        val features = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            setOf(
+                PackageManager.FEATURE_FINGERPRINT,
+                PackageManager.FEATURE_IRIS,
+                PackageManager.FEATURE_FACE
+            )
+        } else {
+            setOf(PackageManager.FEATURE_FINGERPRINT)
+        }
+        return features.any(pm::hasSystemFeature)
+    }
+
+    private fun showWithoutCipher() {
         val executor = ContextCompat.getMainExecutor(this)
         val biometricPrompt = BiometricPrompt(this, executor, authenticationCallback)
         val promptInfo = BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Biometric login for my app")
-                .setSubtitle("Log in using your biometric credential")
-                .setNegativeButtonText("Cancel")
-                .build()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Cancel")
+            .build()
         biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun showWithCipher() {
+        val executor = ContextCompat.getMainExecutor(this)
+        val biometricPrompt = BiometricPrompt(this, executor, authenticationCallback)
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Cancel")
+            .build()
+        val cipher = getCipher()
+        val crypto = BiometricPrompt.CryptoObject(cipher)
+        biometricPrompt.authenticate(promptInfo, crypto)
+    }
+
+    // Source: https://arctouch.com/blog/cryptographic-keys-fingerprint-authentication-android/
+    private fun getCipher(): Cipher {
+        val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+        val masterKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+
+        val keySpec = KeyGenParameterSpec.Builder(
+            masterKeyAlias, // (1)
+            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT // (2)
+        )
+            .setKeySize(256) // (3)
+            .setBlockModes(KeyProperties.BLOCK_MODE_CBC) // (4)
+            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7) // (5)
+//            .setUserAuthenticationRequired(true) // (6)
+            .build()
+        val keygen = KeyGenerator.getInstance(
+            KeyProperties.KEY_ALGORITHM_AES, // (1)
+            "AndroidKeyStore" // (2)
+        ).apply { init(keySpec) } // (3)
+
+        val key: SecretKey = keygen.generateKey() // (4)
+
+        return Cipher.getInstance("AES/CBC/PKCS7Padding")
+            .apply { init(Cipher.ENCRYPT_MODE, key) }
     }
 
     private fun String?.writeResult() {
